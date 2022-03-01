f069dbf8d8b73a7cc812c0e79368492cdc36f17a
daniel.venter@me.com <daniel.venter@me.com>
2022-03-01 17:25:20 +0200

    Formatting

--- rango/models.py
+++ rango/models.py
@@ -1,8 +1,8 @@
 import os
 
 from django.db import models
+
 current_dir = os.getcwd()
-from django.template.defaultfilters import slugify
 
 
 class Restaurant(models.Model):
@@ -10,22 +10,23 @@ class Restaurant(models.Model):
     street_number = models.PositiveIntegerField()
     street = models.CharField(max_length=128)
     city = models.CharField(max_length=128)
-    map_link = models.CharField(max_length=256)
-    ratings = models.JSONField(default=[])
+    ratings = models.JSONField(default=list)
     description = models.CharField(max_length=1500)
     img1 = models.ImageField(upload_to=f"{current_dir}\\media\\")
     img2 = models.ImageField(upload_to=f"{current_dir}\\media\\")
     img3 = models.ImageField(upload_to=f"{current_dir}\\media\\")
     restaurant_id = models.CharField(max_length=128, primary_key=True)
-    comments = models.JSONField(default={})
+    comments = models.JSONField(default=dict)
 
     @property
     def rating(self):
-        return sum(self.ratings) / len(self.ratings)
+        return round(sum(self.ratings) / len(self.ratings), 2)
 
-    def generate_map_link(self):
-        self.map_link = f"{self.street_number}+{self.street}+{self.city}"
-        self.map_link.replace(" ", "+")
+    @property
+    # Map Link used for Google API
+    def map_link(self):
+        map_link = f"{self.street_number}+{self.street.replace(' ', '+')}+{self.city}"
+        return map_link
 
     def __str__(self):
         return self.restaurant_id
@@ -37,7 +38,7 @@ class user_client(models.Model):
     street_number = models.PositiveIntegerField()
     street = models.CharField(max_length=128)
     city = models.CharField(max_length=128)
-    rated_restaurants = models.JSONField(default={})
+    rated_restaurants = models.JSONField(default=dict)
     rates = models.ManyToManyField(Restaurant, related_name="rates")
     password = models.CharField(max_length=24)
     email = models.EmailField()
@@ -47,20 +48,18 @@ class user_client(models.Model):
     owned_restaurants = models.ManyToManyField(Restaurant, related_name="owns")
 
     @property
+    # List generated for easy checking
     def owned_restaurants_list(self):
         owned_restaurants = []
-        for restaurant in user_client.owned_restaurants.all():
-            owned_restaurants.append(restaurant.name)
+        for restaurant in self.owned_restaurants.all():
+            owned_restaurants.append(restaurant.restaurant_id)
         return owned_restaurants
 
     @property
+    # MapLink used for Google APIs
     def map_link(self):
-        map_link = f"{self.street_number}+{self.street}+{self.city}"
-        map_link.replace(" ", "+")
+        map_link = f"{self.street_number}+{self.street.replace(' ', '+')}+{self.city}"
         return map_link
 
     def __str__(self):
         return self.username
-
-
-

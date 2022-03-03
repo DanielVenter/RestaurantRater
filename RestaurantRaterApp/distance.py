c562bc1d0a2487cbf7b637ecfdf71947365bd48e
daniel.venter@me.com <daniel.venter@me.com>
2022-03-03 22:20:27 +0200

    Update distance.py

--- RestaurantRaterApp/distance.py
+++ RestaurantRaterApp/distance.py
@@ -1,11 +1,13 @@
 import requests
 
 API_KEY = "AIzaSyAxJa_f1f5FhqyY_JhZ42JBijy4dXNgGQA"
-url = f"https://maps.googleapis.com/maps/api/distancematrix/json?origins=Boston%2CMA%7&destinations=Lexington%2CMA%7&departure_time=now&key={API_KEY}"
 
-payload={}
-headers = {}
+def get_distance():
+    url = f"https://maps.googleapis.com/maps/api/distancematrix/json?origins=Boston%2CMA%7&destinations=Lexington%2CMA%7&departure_time=now&key={API_KEY}"
 
-response = requests.request("GET", url, headers=headers, data=payload)
+    payload={}
+    headers = {}
 
-print(response.text)
+    response = requests.request("GET", url, headers=headers, data=payload)
+
+    print(response.text)

58632cc3a63bed6419bef6331189d2a6966d5616
AndreiLucianRadulescu <2577007r@student.gla.ac.uk>
2022-03-03 14:58:20 +0000

    commented tests for population script

--- RestaurantRaterApp/tests/test_population_script.py
+++ RestaurantRaterApp/tests/test_population_script.py
@@ -7,6 +7,7 @@ class PopulationScriptTests(TestCase):
     def setUp(self):
         populate()
 
+    #Tests populate_database's likes method
     def test_likes(self):
         msg = "population script's likes method does not work accordingly."
         likes("Colin", "ALC")
@@ -14,6 +15,7 @@ class PopulationScriptTests(TestCase):
         self.assertTrue(Restaurant.objects.get(restaurant_id="ALC") in user_client.objects.get(
             name="Colin").liked_restaurants.all(), msg)
 
+    #Tests if populate_database's rates method adds the rating to the Restaurant's ratings list
     def test_rates_adds_rating_list_size(self):
         msg = "population script's rates method does not work accordingly"
 
@@ -25,6 +27,7 @@ class PopulationScriptTests(TestCase):
 
         self.assertEqual(8, len(Restaurant.objects.get(restaurant_id="ALC").ratings), msg)
 
+    #Tests if Restaurant's rating property calculates a new rating every time, even if the ratings list has changed
     def test_rates_adds_rating_calculation(self):
         msg = "population script's rates method does not work accordingly"
 
@@ -35,6 +38,7 @@ class PopulationScriptTests(TestCase):
         rates("Colin", "ALC")
         self.assertEqual(4.38, Restaurant.objects.get(restaurant_id="ALC").rating, msg)
 
+    #Tests if user_clients that are owners are assigned with at least one owned restaurant
     def test_owners_have_owned_restaurant(self):
         msg = "population script does not give owners at least one restaurant"
 
@@ -43,6 +47,7 @@ class PopulationScriptTests(TestCase):
         for usr in lst:
             self.assertTrue(len(usr.owned_restaurants.all()) > 0, msg)
 
+    #Tests if all user_clients that are not owners have no restaurants owned
     def test_not_owners_have_zero_restaurants(self):
         msg = "population script does give users that are not owners an empty list"
 
@@ -51,6 +56,7 @@ class PopulationScriptTests(TestCase):
         for usr in lst:
             self.assertTrue(len(usr.owned_restaurants.all()) == 0, msg)
 
+    #Tests if populate_database's owns method modifies the owned_restaurants list of the user
     def test_owns_len(self):
         msg = "population script's owns method does not work accordingly"
 
@@ -58,6 +64,7 @@ class PopulationScriptTests(TestCase):
 
         self.assertTrue(len(user_client.objects.get(name="Danny").owned_restaurants.all()) == 6, msg)
 
+    #Tests if populate_database's owns method adds the restaurant to the user's owned_restaurants list
     def test_owns_restaurant_added(self):
         msg = "population script's owns method does not work accordingly"
 
@@ -66,16 +73,19 @@ class PopulationScriptTests(TestCase):
         self.assertTrue(Restaurant.objects.get(restaurant_id="ALC") in user_client.objects.get(
             name="Danny").owned_restaurants.all(), msg)
 
+    #Tests if the population script creates a correct number of restaurants
     def test_correct_no_restaurants(self):
         msg = "population script does not create all restaurants."
 
         self.assertEqual(len(Restaurant.objects.all()), 15, msg)
 
+    #Tests if the population script creates a correct number of users
     def test_correct_no_users(self):
         msg = "population script does not create all users."
 
         self.assertEqual(len(user_client.objects.all()), 11, msg)
 
+    #Tests if the population script creates the correct restaurants
     def test_correct_restaurants_created(self):
         msg = "population script does not create all restaurants."
 
@@ -84,6 +94,7 @@ class PopulationScriptTests(TestCase):
         for restaurant_id in restaurant_ids:
             self.assertIsNotNone(Restaurant.objects.get(restaurant_id=restaurant_id))
 
+    #Tests if the population script creates the correct users
     def test_correct_users_created(self):
         msg = "population script does not create all users."
 

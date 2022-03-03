414cc6d91f336c25dfbfe856a858b67c108fc8c1
daniel.venter@me.com <daniel.venter@me.com>
2022-03-03 16:35:59 +0200

    Renamed two tests with same name

--- RestaurantRaterApp/tests/test_population_script.py
+++ RestaurantRaterApp/tests/test_population_script.py
@@ -2,17 +2,19 @@ from django.test import TestCase
 from populate_database import populate, likes, rates, owns
 from RestaurantRaterApp.models import user_client, Restaurant
 
+
 class PopulationScriptTests(TestCase):
     def setUp(self):
         populate()
-    
+
     def test_likes(self):
         msg = "population script's likes method does not work accordingly."
         likes("Colin", "ALC")
 
-        self.assertTrue(Restaurant.objects.get(restaurant_id="ALC") in user_client.objects.get(name="Colin").liked_restaurants.all(), msg)
-    
-    def test_rates_adds_rating(self):
+        self.assertTrue(Restaurant.objects.get(restaurant_id="ALC") in user_client.objects.get(
+            name="Colin").liked_restaurants.all(), msg)
+
+    def test_rates_adds_rating_list_size(self):
         msg = "population script's rates method does not work accordingly"
 
         usr = user_client.objects.get(name="Colin")
@@ -23,7 +25,7 @@ class PopulationScriptTests(TestCase):
 
         self.assertEqual(8, len(Restaurant.objects.get(restaurant_id="ALC").ratings), msg)
 
-    def test_rates_adds_rating(self):
+    def test_rates_adds_rating_calculation(self):
         msg = "population script's rates method does not work accordingly"
 
         usr = user_client.objects.get(name="Colin")
@@ -40,7 +42,7 @@ class PopulationScriptTests(TestCase):
 
         for usr in lst:
             self.assertTrue(len(usr.owned_restaurants.all()) > 0, msg)
-    
+
     def test_not_owners_have_zero_restaurants(self):
         msg = "population script does give users that are not owners an empty list"
 
@@ -55,25 +57,25 @@ class PopulationScriptTests(TestCase):
         owns("Danny", "ALC")
 
         self.assertTrue(len(user_client.objects.get(name="Danny").owned_restaurants.all()) == 6, msg)
-    
+
     def test_owns_restaurant_added(self):
         msg = "population script's owns method does not work accordingly"
 
         owns("Danny", "ALC")
 
-        self.assertTrue(Restaurant.objects.get(restaurant_id="ALC") in user_client.objects.get(name="Danny").owned_restaurants.all(), msg)
-
+        self.assertTrue(Restaurant.objects.get(restaurant_id="ALC") in user_client.objects.get(
+            name="Danny").owned_restaurants.all(), msg)
 
     def test_correct_no_restaurants(self):
         msg = "population script does not create all restaurants."
 
         self.assertEqual(len(Restaurant.objects.all()), 15, msg)
-    
+
     def test_correct_no_users(self):
         msg = "population script does not create all users."
 
         self.assertEqual(len(user_client.objects.all()), 11, msg)
-    
+
     def test_correct_restaurants_created(self):
         msg = "population script does not create all restaurants."
 
@@ -81,11 +83,12 @@ class PopulationScriptTests(TestCase):
 
         for id in restaurant_ids:
             self.assertIsNotNone(Restaurant.objects.get(restaurant_id=id))
-    
+
     def test_correct_users_created(self):
         msg = "population script does not create all users."
 
-        usernames = ["Mark.E", "Thom.O", "Matt.W", "Michael.G", "Andy.P", "Rose.S", "Jeremy.S", "Jeff.D", "Colin", "Nicola.H", "Danny.M"]
+        usernames = ["Mark.E", "Thom.O", "Matt.W", "Michael.G", "Andy.P", "Rose.S", "Jeremy.S", "Jeff.D", "Colin",
+                     "Nicola.H", "Danny.M"]
 
         for username in usernames:
             self.assertIsNotNone(user_client.objects.get(username=username))

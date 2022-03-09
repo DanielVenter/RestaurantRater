034b96815dd6891860e2ac57f1905fd43f7603fb
AndreiLucianRadulescu <2577007r@student.gla.ac.uk>
2022-03-09 17:52:28 +0000

    deleted userprofileform

--- RestaurantRaterApp/forms.py
+++ RestaurantRaterApp/forms.py
@@ -2,7 +2,7 @@ import os
 from django import forms
 from django.db import models
 from django.core.exceptions import ValidationError
-from RestaurantRaterApp.models import UserProfile, Restaurant, user_client
+from RestaurantRaterApp.models import Restaurant, user_client
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.forms import PasswordChangeForm

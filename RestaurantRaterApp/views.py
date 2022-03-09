8e78c0a8bc11357fc6e086b726983d7a293fedf6
Nash Waugh <nwaugh444@gmail.com>
2022-03-09 02:57:18 +0000

    changes to views
    fixed some code in profile view
    
    removed some unnecessary code left over from rango project

--- RestaurantRaterApp/views.py
+++ RestaurantRaterApp/views.py
@@ -121,7 +121,7 @@ def profile(request):
     for restaurant in restaurants:
         for com in restaurant.comments:
             if com == this_user.username:
-                users_comments[restaurant.comments[com]]
+                users_comments[restaurant.name] = restaurant.comments[com]
     context_dict['comments']= users_comments
     
     return render(request, 'RestaurantRaterApp/profile.html', context=context_dict)
@@ -208,26 +208,3 @@ def user_login(request):
 def user_logout(request):
     logout(request)
     return redirect(reverse('RestaurantRaterApp:home'))
-        
-def get_server_side_cookie(request, cookie, default_val=None):
-    val = request.session.get(cookie)
-    if not val:
-        val = default_val
-    return val
-
-
-def visitor_cookie_handler(request):
-    visits = int(get_server_side_cookie(request, 'visits', '1'))
-    last_visit_cookie = get_server_side_cookie(request,
-                                               'last_visit',
-                                               str(datetime.now()))
-    last_visit_time = datetime.strptime(last_visit_cookie[:-7],
-                                        '%Y-%m-%d %H:%M:%S')
-
-    if (datetime.now() - last_visit_time).days > 0:
-        visits += 1
-        request.session['last_visit'] = str(datetime.now())
-    else:
-        request.session['last_visit'] = last_visit_cookie
-
-    request.session['visits'] = visits

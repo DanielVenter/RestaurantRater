0fe22aa616170547d566de433da2c2ed3245fefd
daniel.venter@me.com <daniel.venter@me.com>
2022-03-13 18:48:15 +0200

    Updated File saving schema

--- RestaurantRaterApp/models.py
+++ RestaurantRaterApp/models.py
@@ -20,11 +20,11 @@ class Restaurant(models.Model):
     ratings = models.JSONField(default=list)
     description = models.CharField(max_length=240)
     img1 = ResizedImageField(size=[225, 225], quality=100, crop=["middle", "center"],
-                             upload_to=f"{current_dir}\\media\\", force_format='jpeg')
+                             upload_to=f"{current_dir}/media/", force_format='jpeg')
     img2 = ResizedImageField(size=[225, 225], quality=100, crop=["middle", "center"],
-                             upload_to=f"{current_dir}\\media\\", force_format='jpeg')
+                             upload_to=f"{current_dir}/media/", force_format='jpeg')
     img3 = ResizedImageField(size=[225, 225], quality=100, crop=["middle", "center"],
-                             upload_to=f"{current_dir}\\media\\", force_format='jpeg')
+                             upload_to=f"{current_dir}/media/", force_format='jpeg')
     restaurant_id = models.CharField(max_length=128, primary_key=True)
     comments = models.JSONField(default=dict)
 
@@ -78,8 +78,10 @@ class user_client(models.Model):
         user = user_client.objects.get(user=self.user)
         distances_matrix = user.distances_dict
         restaurants = []
+
         start = f"{user.street_number} {user.street} {user.city}"
         end = []
+
         if not new_address:
             for restaurant in Restaurant.objects.all():
                 end.append(f"{restaurant.street_number} {restaurant.street} {restaurant.city}")

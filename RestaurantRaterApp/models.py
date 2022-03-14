44b17960f9112f670bfa210102d6e4faf45c73f0
daniel.venter@me.com <daniel.venter@me.com>
2022-03-14 17:16:54 +0200

    os.path.join()

--- RestaurantRaterApp/models.py
+++ RestaurantRaterApp/models.py
@@ -20,11 +20,11 @@ class Restaurant(models.Model):
     ratings = models.JSONField(default=list)
     description = models.CharField(max_length=240)
     img1 = ResizedImageField(size=[225, 225], quality=100, crop=["middle", "center"],
-                             upload_to=f"{current_dir}/media/", force_format='jpeg')
+                             upload_to=os.path.join(current_dir, "media"), force_format='jpeg')
     img2 = ResizedImageField(size=[225, 225], quality=100, crop=["middle", "center"],
-                             upload_to=f"{current_dir}/media/", force_format='jpeg')
+                             upload_to=os.path.join(current_dir, "media"), force_format='jpeg')
     img3 = ResizedImageField(size=[225, 225], quality=100, crop=["middle", "center"],
-                             upload_to=f"{current_dir}/media/", force_format='jpeg')
+                             upload_to=os.path.join(current_dir, "media"), force_format='jpeg')
     restaurant_id = models.CharField(max_length=128, primary_key=True)
     comments = models.JSONField(default=dict)
 
